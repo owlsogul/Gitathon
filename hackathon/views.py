2438f79f5e9bd090b972503ed400c694ed453334
wkdthf21 <wkdthf21@naver.com>
2018-10-13 17:45:56 +0900

    add function of holding hackathon

--- hackathon/views.py
+++ hackathon/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from hackathon.forms import *
 
 # Create your views here.
+
+# 대회방 개설
+def holdHackathon(request):
+    form = Form()
+
+    if request.method == 'POST':
+        form = Form(request.POST)
+        if form.is_valid():
+            # form을 DB에 저장
+            form.save()
+    else:
+            form = Form()
+
+    return render(request, 'hold.html',{'form':form})
