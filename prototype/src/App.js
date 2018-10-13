6dfb3f62998a10228ba47371389ad409294db4fa
tyg04308 <tyg04308@naver.com>
2018-10-13 20:08:37 +0900

    Make 'MakeTeam' Page

--- prototype/src/App.js
+++ prototype/src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
 import NaviMenu from './NaviMenu';
+import MakeTeam from './MakeTeam';
 
 const metaInfo = [
-  {
-    gitathonLogo : "/img/gitathonLogo.png",
-    userName : "장예솔",
-    hackathonLogo : "/img/hackathonLogo.png",
-    profileLogo : "/img/profileLogo.png"
-  }
+    {
+      gitathonLogo : "/img/gitathonLogo.png",
+      userName : "장예솔",
+      hackathonLogo : "/img/hackathonLogo.png",
+      profileLogo : "/img/profileLogo.png"
+    }
 ]
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <NaviMenu
-          gitathonLogo={metaInfo[0].gitathonLogo}
-          userName={metaInfo[0].userName}
-          hackathonLogo = {metaInfo[0].hackathonLogo}
-          profileLogo={metaInfo[0].profileLogo}
-        />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <NaviMenu
+                    gitathonLogo={metaInfo[0].gitathonLogo}
+                    userName={metaInfo[0].userName}
+                    hackathonLogo = {metaInfo[0].hackathonLogo}
+                    profileLogo={metaInfo[0].profileLogo}
+                />
+                <br/><br/><br/>
+                <MakeTeam />
+            </div>
+        );
+    }
 }
 
 export default App;
