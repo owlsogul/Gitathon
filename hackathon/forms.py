61b6b7983b7da8f7e5bab85c17ad668d58cf2c29
wkdthf21 <wkdthf21@naver.com>
2018-10-13 17:42:52 +0900

    add forms.py

---
+++ hackathon/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+from hackathon.models import *
+
+class Form(ModelForm):
+    class Meta:
+        model = hackathonInformation
+        # fields = '__all__'
+        # exclude = ['title']
+        fields = ('title', 'applyDate_start', 'applyTime_start', 'applyDate_end', 'applyTime_end', 'peopleNum', 'memberNum_max', 'memberNum_min', 'selectMatching', 'Images', 'text', )
