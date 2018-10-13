f3e5a5e0978c742f7f8a5eb01f4b1d1518aa29b5
wkdthf21 <wkdthf21@naver.com>
2018-10-13 23:11:57 +0900

    make participate model DB Schema

--- hackathon/models.py
+++ hackathon/models.py
@@ -20,4 +20,12 @@ class hackathonInformation(models.Model):
     Images = models.FileField(blank=True, upload_to='./hackathon/static/uploads')
     text = models.TextField(blank=True)
     created_date = models.DateTimeField(auto_now = True)
+    # 회원정보 아직 없어서 default로 wkdthf21
     hackathonHost = models.CharField(max_length = 100, default = "wkdthf21")
+
+class participate(models.Model):
+    # 회원정보 아직 없어서 default로 wkdthf21
+    memberID = models.CharField(max_length = 100, default = "wkdthf21")
+    # participate의 hackathonID는 hackathonInformation ID를 참조
+    # 해커톤 대회 정보가 지워지면 자동으로 participate의 정보도 지워진다.
+    hackathonID = models.ForeignKey(hackathonInformation, on_delete=models.CASCADE)
