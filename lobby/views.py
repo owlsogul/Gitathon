edc51d26c9e7b591bbcfffb340870188ec8c6e60
owlsogul <owlsogul@naver.com>
2018-10-16 07:25:14 +0900

    메인 페이지 팀플 목록 나오게

--- lobby/views.py
+++ lobby/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import redirect, render
+from teamproject.models import Team
+from accounts.models import Member, Participate
 
 # Create your views here.
 def login(request):
@@ -14,7 +16,18 @@ def main(request):
     if not 'memberId' in request.session:
         return redirect('/lobby')
     else:
-        return render(request, 'lobby/main.html', {'memberId':request.session['memberId']})
+        member = Member.objects.filter(memberId=request.session['memberId'])
+        print(member)
+        joinIds = []
+        for id in Participate.objects.filter(memberId=member[0]).values('teamId'):
+            joinIds.append(id['teamId'])
+        print(joinIds)
+        joinTeams = Team.objects.filter(id__in=joinIds)
+        print(joinTeams)
+        return render(request, 'lobby/main.html', {
+            'memberId':request.session['memberId'],
+            'teams':joinTeams,
+        })
 
 def signup(request):
     return lobby(request)
