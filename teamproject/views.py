392a6f60fa6881ad5f48ce374aa1331676fb546a
owlsogul <owlsogul@naver.com>
2018-10-16 04:17:45 +0900

    html 확장해서 쓸수 있게 base.html 만들어서 확장

--- teamproject/views.py
+++ teamproject/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 def main(request, teamId):
-    return render(request, 'teamproject/main.html', {'teamId':teamId})
+    if not 'memberId' in request.session:
+        return redirect('/lobby')
+    else:
+        return render(request, 'teamproject/main.html', {
+            'memberId':request.session['memberId'],
+            'teamId':teamId,
+        })
 
 def create(request):
-    return render(request, 'teamproject/create.html', {})
+    if not 'memberId' in request.session:
+        return redirect('/lobby')
+    else:
+        return render(request, 'teamproject/create.html', {
+            'memberId':request.session['memberId'],
+        })
 
 def create_process(request):
     return render(request, 'teamproject/create.html', {})
