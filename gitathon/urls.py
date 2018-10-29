e210d6123533c4cf7bd7bc97085c212bc070f710
tyg04308 <tyg04308@naver.com>
2018-10-29 17:11:55 +0000

    some test

--- gitathon/urls.py
+++ gitathon/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('lobby/', include('lobby.urls')),
     path('team/', include('teamproject.urls')),
     path('accounts/', include('accounts.urls')),
+	path('well-known/', include('letsencrypt.urls')),
 ]
