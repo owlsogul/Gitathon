f0b8667e01d174b6f1ff9254862121c154b1e8be
wkdthf21 <wkdthf21@naver.com>
2018-10-30 13:19:22 +0900

    Update urls.py

--- teamproject/urls.py
+++ teamproject/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('create', views.create, name='create'),
     url(r'^create/(?P<hackId>\d+)/$', views.createWithHackId, name='create'),
+    url(r'^create/(?P<hackId>\d+)/process_create/$', views.process_create, name='process_create'),
     path('process_create', views.process_create, name='process_create'),
     path('<teamId>/main', views.main, name='main'),
     path('<teamId>/notice', views.notice, name='notice'),
