7dd603fc0da6813e948adf39cc429ec4b701cb45
owlsogul <owlsogul@naver.com>
2018-11-05 18:00:44 +0900

    오류 수정

--- chat/views.py
+++ chat/views.py
@@ -6,5 +6,5 @@ def index(request):
 
 def room(request, room_name):
     return render(request, 'chat/room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name))
+        'room_name_json': room_name
     })
