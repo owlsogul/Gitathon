8ddc531323fddaa00822d58bb5daf3101e431878
owlsogul <owlsogul@naver.com>
2018-11-06 09:44:52 +0900

    test

--- teamproject/views.py
+++ teamproject/views.py
@@ -52,7 +52,6 @@ def contribution(request, teamId):
             'comment':teamContribution.comment,
             'code':teamContribution.code,
             'resource':teamContribution.resource,
-            'test':'test',
         })
 
 def chat(request, teamId):
