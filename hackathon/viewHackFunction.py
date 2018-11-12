b70e0d5b36fba3cd8078027339a7eb934a7df449
wkdthf21 <wkdthf21@naver.com>
2018-11-13 05:04:14 +0900

    create gitEval function

--- hackathon/viewHackFunction.py
+++ hackathon/viewHackFunction.py
@@ -69,3 +69,45 @@ def nomalization(c,l,b,t):
 
     except:
         raise Exception("정규화 중 오류가 발생하였습니다.")
+
+
+        
+
+# git 활용도 점수 계산하는 함수
+# 각 항목별 비율과
+# 전체 팀들의 평균과 표준편차 필요
+# 선택된 팀의 각 항목별 값도 필요
+
+def gitEval(commitRate,lineRate,branchRate,teamRate, avgTotalData, stdTotalData, teamRawData) :
+
+
+    # 입력한 수를 기준으로 비율 계산
+    # 소수점 둘 째자리로 반올림
+    # percentage = [commitRate, lineRate, branchRate, teamRate]
+    percentage = []
+    percentage = calPercent(commitRate,lineRate,branchRate,teamRate)
+
+
+    # 전체 평균 대비 한 팀의 commit수, 수정된 줄 수, merge된 branch 수, 팀원 기여도 점수 계산하기
+    # 표준화
+    teamRawData[0] = (teamRawData[0] - avgTotalData[0]) / stdTotalData[0] # -5 0.81
+    teamRawData[1] = (teamRawData[1] - avgTotalData[1]) / stdTotalData[1] # -27.7 0.0
+    teamRawData[2] = (teamRawData[2] - avgTotalData[2]) / stdTotalData[2] # 0.18 1.0
+    teamRawData[3] = (teamRawData[3] - avgTotalData[3]) / stdTotalData[3] # -1.17 0.95
+
+    # 정규화
+    nomalData=[]
+    nomalData = nomalization(teamRawData[0],teamRawData[1],teamRawData[2],teamRawData[3])
+
+    # 계산
+
+    gitScore = 0
+
+    for i in range(0,4) :
+
+        gitScore += round(percentage[i] * nomalData[i], 2)
+
+        # 최종 결과값
+    gitScore = gitScore * 100
+
+    return gitScore
