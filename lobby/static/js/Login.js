ede5551ca2e93fd8e1a41474741ed46bd84fcf69
tyg04308 <tyg04308@naver.com>
2018-11-18 16:03:45 +0900

    bundle build sucessful

--- lobby/static/js/Login.js
+++ lobby/static/js/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Login.css';
 
 function Login ({gitathonLogo, hackNum, teamNum}){
     return (
---
+++ node_modules/babel-plugin-css-modules-transform/build/utils/requireLocalFileOrNodeModule.js
@@ -0,0 +1,26 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = requireLocalFileOrNodeModule;
+
+var _path = require('path');
+
+/**
+ * Require local file or node modules
+ *
+ * @param {String} path
+ * @returns {*}
+ */
+function requireLocalFileOrNodeModule(path) {
+    const localFile = (0, _path.resolve)(process.cwd(), path);
+
+    try {
+        // first try to require local file
+        return require(localFile);
+    } catch (e) {
+        // try to require node_module
+        return require(path);
+    }
+}
