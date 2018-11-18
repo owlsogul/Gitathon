d6a86da6129160a35e8afcacca1f4fd80aea73ef
tyg04308 <tyg04308@naver.com>
2018-11-18 17:53:54 +0900

    adding img

--- lobby/static/js/App.js
+++ lobby/static/js/App.js
@@ -3,10 +3,10 @@ import LoginMain from './Login';
 
 const metaInfo = [
     {
-      gitathonLogo : "127.0.0.1:8000/img/gitathonLogo.png",
+      gitathonLogo : "../../../img/gitathonLogo.png",
       userName : "wkdthf21",
-      hackathonLogo : "127.0.0.1:8000/img/hackathonLogo.png",
-      profileLogo : "127.0.0.1:8000/img/profileLogo.png"
+      hackathonLogo : "/../../../img/hackathonLogo.png",
+      profileLogo : "/../../../img/profileLogo.png"
     }
 ]
 
