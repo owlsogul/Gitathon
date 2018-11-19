ceffac2fd4e0137e78ad5679d68d1715c53a84fb
wkdthf21 <wkdthf21@naver.com>
2018-11-20 02:38:32 +0900

    add hackUsability model

--- hackathon/admin.py
+++ hackathon/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import HackathonInformation
-from .models import HackNotice
+from .models import *
 
 # Register your models here.
 admin.site.register(HackathonInformation)
 admin.site.register(HackNotice)
+admin.site.register(HackUsability)
