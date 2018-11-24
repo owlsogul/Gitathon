d1af9ece71ddb7fb7e20377c75f2957cdcc4cd6a
wkdthf21 <wkdthf21@naver.com>
2018-11-25 05:29:01 +0900

    fix showDiffCommit func

--- pyModule/abuse.py
+++ pyModule/abuse.py
@@ -6,15 +6,18 @@ old_path = ""
 
 def showDiffCommit(hackName, teamName, commitId):
 
+    #path = "C:\\Users\\장예솔\\Desktop\\Yedori\\중앙대\\3학년\\2학기\\캡스톤\\Source\\Gitathon"
+    #os.chdir(path)
 	changeDir(hackName, teamName)
+    print('실행')
+    try:
+        output = subprocess.check_output('git show ' + commitId, shell=True).decode()
+        output.encode()
 
-	try:
-		result = subprocess.check_output('git show' + commitId, shell=True).decode()
-		result.encode()
+        print(output)
 
 
-	except subprocess.CalledProcessError as e:
-		print ("Error //  ", e.output)
-		os.chdir(old_path)
+    except subprocess.CalledProcessError as e:
+        print("Error //" , e.output)
 
-	return result
+    return output
