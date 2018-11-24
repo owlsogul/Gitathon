48f8b67986857b956202d399516e828f622c3d38
tyg04308 <tyg04308@naver.com>
2018-11-25 00:44:07 +0900

    modify countMergedBracnh func

--- pyModule/gitBranch.py
+++ pyModule/gitBranch.py
@@ -34,18 +34,25 @@ def countMergedBranch(hackName, teamName):
 
 	try:
 		old_branch = getCurrentBranch()
+                branchs = []
 		os.system('git checkout master')
-		result = subprocess.check_output('git branch -r --merged', shell=True).decode()
+		result = subprocess.check_output('git log --merges --oneline', shell=True).decode()
 		result.encode()
 
 		for line in result.split('\n'):
-			words = line.replace('->', '/').replace(' ','').split('/')
+			words = line.replace('->', '/').split()
 
-			if len(words) < 2:
+			if len(words) == 0:
 				continue
 
-#			if words[1] != "HEAD":
-			mergedBranch = mergedBranch + 1
+			if words[1] != "Merge":
+                            if words[2] == "branch":
+                                if words[3] in branchs:
+                                    continue
+                                else:
+                                    branchs.append(words[3])
+                                    mergedBranch = mergedBranch + 1
+                                
 
 	except subprocess.CalledProcessError as e:
 		print("Error //  ", e.output)
