0b1f1b03fb177e763923032c0ed37c8fc126ca78
tyg04308 <tyg04308@naver.com>
2018-11-27 12:31:00 +0900

    mingyu ver test

--- lobby/static/js/Login.js
+++ lobby/static/js/Login.js
@@ -7,32 +7,19 @@ class LoginMain extends Component{
     }
 
     render(){
-        {/*
-        let hackNum = 0;
-        let teamPNum = 0;
-        items(this.props.numData, (key, value) => {
-            if (key == "numHackathon"){
-                hackNum = value
-            }
-            else if (key  == "numTeamproject"){
-                teamNum = value
-            }
-        })
-        */}
-        console.log(this.props)
-        return(
-            <div className="LoginScreen">
-            if this.state.mode == 'Login'{
-                <Login
-                    gitathonLogo = {this.props.gitathonLogo}
-                    hackNum = {hackNum}
-                    teamNum = {teamNum} />
-            }
-            else if {
-                <Register />
-            }
-            </div>
-        );
+        <div class="login-form">
+          <form class="" action="/accounts/login" method="POST">
+            <input type="text" name="memberId" placeholder="아이디를 입력해주세요" /><br />
+            <input type="password" name="password" placeholder="비밀번호를 입력해주세요" /><br />
+            <input type="submit" name="" value="함께하기" />
+          </form>
+          <form class="" action="/accounts/register" method="POST">
+            <input type="text" name="memberId" placeholder="아이디를 입력해주세요" /><br />
+            <input type="text" name="email" placeholder="이메일을 입력해주세요" /><br />
+            <input type="password" name="password" placeholder="비밀번호를 입력해주세요" /><br />
+            <input type="submit" name="" value="가입하기" />
+          </form>
+        </div>
     }
 }
 
