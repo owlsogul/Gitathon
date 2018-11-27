80f1a824ace8b36f2702b86ef1c55efec83e9568
oxo1996 <44669920+oxo1996@users.noreply.github.com>
2018-11-27 17:26:54 +0900

    test

--- lobby/views.py
+++ lobby/views.py
@@ -6,7 +6,7 @@ from hackathon.models import *
 # Create your views here.
 def login(request):
     if not 'memberId' in request.session:
-        result = list(map(['numHackathon', 'numTeamproject'], [1234, 30000]))
+        result ={'numHackathon': 1234, 'numTeamproject': 30000}
         return render(request, 'lobby/login.html', result)
     else:
         return redirect('/lobby/main')
