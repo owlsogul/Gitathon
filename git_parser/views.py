d15718647f1d98b5f57c1523c2a7407a53d52b3b
owlsogul <owlsogul@naver.com>
2018-12-04 13:28:34 +0900

    view 누락된거 수정

--- git_parser/views.py
+++ git_parser/views.py
@@ -4,6 +4,44 @@ from django.http import JsonResponse
 from teamproject.models import *
 from accounts.models import *
 
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from logging import getLogger
+
+from .tasks import test, lookCommit, setLookCommit
+
+logger = getLogger(__name__)
+
+@csrf_exempt
+def tasks(request):
+    if request.method == 'POST':
+        return _post_tasks(request)
+    else:
+        return JsonResponse({}, status=405)
+
+def _post_tasks(request):
+    message = request.POST['message']
+    logger.debug('calling demo_task. message={0}'.format(message))
+    test(message, repeat = 20)
+    return JsonResponse({}, status=302)
+
+
+## abusing ##
+@csrf_exempt
+def startTasks(request):
+    if request.method == 'POST':
+        return catchAbusing(request)
+    else:
+        return JsonResponse({}, status=405)
+
+
+def catchAbusing(request):
+    message = request.POST['message']
+    setLookCommit()
+    lookCommit(message, repeat=30)
+    return JsonResponse({}, status=302)
+
 # Create your views here.
 def get_commit_with_member(request):
     if request.method == 'POST':
