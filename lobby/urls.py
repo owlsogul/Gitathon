8adb9f6158c807a4f65ebc952e98eac04bd9371a
tyg04308 <tyg04308@naver.com>
2018-12-04 10:16:36 +0900

    link to register

--- lobby/urls.py
+++ lobby/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.login, name='login'),
     path('main', views.main, name='main'),
-    path('join', views.join, name='join'),
+    url('join', views.join, name='join'),
 ]
