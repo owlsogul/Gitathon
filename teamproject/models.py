893b6e5e85c4453d17b3285694a568ed022db247
tyg04308 <tyg04308@naver.com>
2018-12-04 14:28:31 +0900

    change teamproject vies

--- teamproject/models.py
+++ teamproject/models.py
@@ -1,58 +1,86 @@
 from django.db import models
+
 from django.utils import timezone
 
+
 # Create your models here.
+
 class Team(models.Model):
-    teamName = models.CharField(max_length=100)
-    leaderId = models.ForeignKey('accounts.Member', on_delete=models.CASCADE)
+
+????teamName = models.CharField(max_length=100)
+
+????leaderId = models.ForeignKey('accounts.Member', on_delete=models.CASCADE)
+
 
 class TeamContribution(models.Model):
-    teamId = models.ForeignKey('teamproject.Team', on_delete=models.CASCADE)
-    comment = models.FloatField(default=0.0)
-    code = models.FloatField(default=0.0)
-    resource = models.FloatField(default=0.0)
-    std_score = models.FloatField(default=0.0)
+
+????teamId = models.ForeignKey('teamproject.Team', on_delete=models.CASCADE)
+
+????comment = models.FloatField(default=0.0)
+
+????code = models.FloatField(default=0.0)
+
+????resource = models.FloatField(default=0.0)
+
+????std_score = models.FloatField(default=0.0)
+
 
 class TeamNotice(models.Model):
-    teamId = models.ForeignKey('teamproject.Team', on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    writer = models.ForeignKey('accounts.Member', on_delete=models.CASCADE)
-    writtenDate = models.DateTimeField('date registered', default=timezone.now())
 
-    def post(self):
-        self.writtenDate = timezone.now()
-        self.save()
+????teamId = models.ForeignKey('teamproject.Team', on_delete=models.CASCADE)
+
+????title = models.CharField(max_length=100)
+
+????content = models.TextField()
+
+????writer = models.ForeignKey('accounts.Member', on_delete=models.CASCADE)
+
+????writtenDate = models.DateTimeField('date registered', default=timezone.now())
+
+
+????def post(self):
+
+????????self.writtenDate = timezone.now()
+
+????????self.save()
+
 
 class TeamChat(models.Model):
-    teamId = models.ForeignKey('teamproject.Team', on_delete=models.CASCADE)
-    sender = models.ForeignKey('accounts.Member', on_delete=models.SET_NULL, null=True)
-    sendedDate = models.DateTimeField('data sended', default=timezone.now())
-    chatMsg = models.TextField()
+
+????teamId = models.ForeignKey('teamproject.Team', on_delete=models.CASCADE)
+
+????sender = models.ForeignKey('accounts.Member', on_delete=models.SET_NULL, null=True)
+
+????sendedDate = models.DateTimeField('data sended', default=timezone.now())
+
+????chatMsg = models.TextField()
+
 
 class TeamMergeRequest(models.Model):
-    teamId = models.ForeignKey('teamproject.Team', on_delete=models.CASCADE)
-    fromBranch = models.CharField(max_length=100)
-    toBranch = models.CharField(max_length=100)
+
+????teamId = models.ForeignKey('teamproject.Team', on_delete=models.CASCADE)
+
+????fromBranch = models.CharField(max_length=100)
+
+????toBranch = models.CharField(max_length=100)
+
 
 class TeamVote(models.Model):
-    requestId = models.ForeignKey('teamproject.TeamMergeRequest', on_delete=models.CASCADE)
-    memberId = models.ForeignKey('accounts.Member', on_delete=models.CASCADE)
-    isAgree = models.BooleanField(default=False)
 
+????requestId = models.ForeignKey('teamproject.TeamMergeRequest', on_delete=models.CASCADE)
 
-class Git(models.Model):
-    teamId = models.ForeignKey('teamproject.Team', on_delete=models.CASCADE)
+????memberId = models.ForeignKey('accounts.Member', on_delete=models.CASCADE)
 
-class Branch(models.Model):
-    branchName = models.CharField(max_length=100)
-    containedGit = models.ForeignKey('teamproject.Git', on_delete=models.CASCADE)
+????isAgree = models.BooleanField(default=False)
+
 
 class Commit(models.Model):
-    containedBranch = models.ForeignKey('teamproject.Branch', on_delete=models.CASCADE)
-    teamId = models.ForeignKey('teamproject.Team', on_delete=models.CASCADE)
-    commitId = models.CharField(max_length=100, primary_key=True)
-    author = models.CharField(max_length=100)
-    code = models.IntegerField()
-    comment = models.IntegerField()
-    resource = models.IntegerField()
+
+????commitId = models.CharField(max_length=100, primary_key = True)
+
+????teamId = models.ForeignKey('teamproject.Team', on_delete=models.CASCADE)
+
+????code = models.IntegerField()
+
+????comment = models.IntegerField()
+
