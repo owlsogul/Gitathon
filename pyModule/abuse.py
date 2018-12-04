44183100f9636cf2034d128698c771f86794a9e4
owlsogul <owlsogul@naver.com>
2018-12-04 13:17:55 +0900

    인덴테이션 수정

--- pyModule/abuse.py
+++ pyModule/abuse.py
@@ -8,8 +8,7 @@ def showDiffCommit(hackName, teamName, commitId):
 
     #path = "C:\\Users\\장예솔\\Desktop\\Yedori\\중앙대\\3학년\\2학기\\캡스톤\\Source\\Source\\capstone\\Gitathon"
     #os.chdir(path)
-	changeDir(hackName, teamName)
-    print('실행')
+	changeDir(hackName, teamName)os
     try:
         output = subprocess.check_output('git show ' + commitId, shell=True).decode()
         output.encode()
