3d967fd22e0dd27281c1d653d26acf9e2a98b188
owlsogul <owlsogul@naver.com>
2018-12-11 02:58:23 +0900

    팀프로젝트 모델 임포트

--- lobby/views.py
+++ lobby/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from teamproject.models import Team
+from teamproject.models import *
 from accounts.models import Member, Participate
 from hackathon.models import *
 
